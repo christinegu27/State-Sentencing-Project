271d1b54e9e1ea2c30c81fe54d8bc9a4aad08c79
christinegu27 <christinegu27@ucla.edu>
2021-04-27 22:52:14 -0700

    Added working spyder and generated csv
    current spyder will get case details for all matching cases from search field (as long as case has a sentence)

---
+++ Web Scraping/cases_week5.py
@@ -0,0 +1,85 @@
+import scrapy
+
+# a general idea of what the spider will eventually do
+# definitely do not crawl - everything is basically pseudocode and nothing actually works as intended
+
+class case_scraper(scrapy.Spider):
+	name = "cases"
+
+	start_urls = ["https://eapps.courts.state.va.us/ocis/search"]
+
+	searchRequestField = {
+			"courtLevels":["C"],
+			"divisions":["Criminal/Traffic"],
+			"selectedCourts":[],
+			#"searchString":[name here],
+			"searchBy":"N"
+			#"endingIndex":0
+		}
+
+	#accepts terms and conditions
+	def parse(self, response):
+		yield scrapy.Request(
+			url = "https://eapps.courts.state.va.us/ocis-rest/api/public/termsAndCondAccepted",
+			callback = self.search)
+
+	#enters search terms
+	def search(self, response):
+		case_scraper.searchRequestField["endingIndex"] = 0
+		#eventually, will have to do some type of looping with the search string to hit all possible names
+		case_scraper.searchRequestField["searchString"] = ['karen miller']
+		yield scrapy.http.JsonRequest(
+			url = "https://eapps.courts.state.va.us/ocis-rest/api/public/search",
+			method = "POST",
+			data = case_scraper.searchRequestField,
+			callback = self.parse_cases)
+	
+	#sends request to access case details for all 30-ish results generated
+	def parse_cases(self, response):
+		case_results = response.json()['context']['entity']['payload']['searchResults']
+		for case in case_results:
+			yield scrapy.http.JsonRequest(
+				url = "https://eapps.courts.state.va.us/ocis-rest/api/public/getCaseDetails",
+				method = "POST",
+				data = case,
+				callback = self.case_details)
+
+		#repeats for all matching cases based on search criteria
+		if 'hasMoreRecords' in response.json()['context']['entity']['payload']:
+			#somehow add +30 to endingIndex from searchRequestField, then
+			case_scraper.searchRequestField['endingIndex'] += 30
+			yield scrapy.http.JsonRequest(
+				url = "https://eapps.courts.state.va.us/ocis-rest/api/public/search",
+				method = "POST",
+				data = case_scraper.searchRequestField,
+				callback = self.parse_cases)
+
+	def case_details(self, response):
+		case_details = response.json()['context']['entity']['payload'] 
+
+		#skip case if no sentence given
+		try:
+			case_details['sentencingInformation']
+		except KeyError:
+			return
+
+		sentence=case_details['sentencingInformation']['sentence']
+
+		try:
+			judge = case_details['caseHearing'][0]['hearingJudge']['judicialOfficialBarMembership']['judicialOfficialBarIdentification']['identificationID']
+		except KeyError:
+			judge = "Unknown"
+
+		yield{
+			'Case Number': case_details['caseTrackingID'],
+			'Name': case_details['caseParticipant'][0]['contactInformation']['personName']['fullName'],
+			'Court': case_details['caseCourt']['fipsCode'],
+			'Last Hearing Date': case_details['caseHearing'][0]['courtActivityScheduleDay']['scheduleDate'],
+			'Charge':case_details['caseCharge']['originalCharge']['chargeDescriptionText'],
+			'Sentence Y': sentence.get('years'),
+			'Sentence M': sentence.get('months'),
+			'Sentence D': sentence.get('days'),
+			'Race': case_details['caseParticipant'][0]['personalDetails']['race'],
+			'Gender': case_details['caseParticipant'][0]['personalDetails']['gender'],
+			'Judge': judge
+			}
