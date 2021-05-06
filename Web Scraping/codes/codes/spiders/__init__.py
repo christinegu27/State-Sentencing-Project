00218145cb0867a943983edecef419548c87294a
Hinal <81390556+hinalsj@users.noreply.github.com>
2021-05-06 09:14:24 -0700

    Scraper and csv containing the codes dictionary

---
+++ Web Scraping/codes/codes/spiders/__init__.py
@@ -0,0 +1,33 @@
+import scrapy
+from scrapy.shell import inspect_response
+import copy
+
+# a general idea of what the spider will eventually do
+# definitely do not crawl - everything is basically pseudocode and nothing actually works as intended
+
+class case_scraper(scrapy.Spider):
+	name = "codes"
+
+	start_urls = ["https://eapps.courts.state.va.us/ocis-rest/api/public/getCourtsCodeDetails"]
+
+	#accepts terms and conditions
+	def parse(self, response):
+		yield scrapy.Request(
+			url = "https://eapps.courts.state.va.us/ocis-rest/api/public/getCourtsCodeDetails",
+			callback = self.code_details)
+
+	def code_details(self, response):
+		code_details = response.json()['context']['entity']['payload']
+		for codes in code_details:
+			try:
+				codes['description']
+			except KeyError:
+				codes['description'] = "No description" 
+
+
+			yield{
+				'Court Category': codes['courtCategory'],
+				'Code ID': codes['codeId'],
+				'Code Description': codes['description'],
+				'Code Type': codes['codeType'],
+				}			
