c3c25858024f5a7df12a2b0f8b26424045ff7d4b
christinegu27 <christinegu27@ucla.edu>
2021-05-28 22:13:31 -0700

    began editing accomack judge scraper, cleaning up old files

--- Web Scraping/judgescraper/acco/spiders/__init__.py
+++ Web Scraping/judgescraper/acco/spiders/__init__.py
@@ -1,16 +1,31 @@
 import scrapy
 
 class JudgeSpider(scrapy.Spider):
-	name = "accomack"
-	start_urls = ["https://web.archive.org/web/20050409104858/http://www.courts.state.va.us/courts/circuit/Accomack/home.html"]
+	name = "judges"
+	def __init__(self, court_url):
+		super(scrapy.Spider, self)__init()
+		self.court_url = court_url
+
+	start_urls = [f"https://web.archive.org/web/20050313093326/http://www.courts.state.va.us:80/courts/circuit/{self.court_url}/home.html"]
 
 	def parse(self, response):
 		year = response.css("td.c")[2].attrib["title"][-4:]
+
+		# post 2005 website
 		for judge in response.css("li.judges").css("::text").extract():
 			yield {
 				"Judge": judge,
 				"Year":year
 			}
+
+		# pre 2005 website
+		for row in response.xpath('//*[@width = "170"]//td[@width="164"]//text()').extract():
+			if "Hon" in row:
+				judge = row[3:-2]
+				yield {
+					"Court": self.court,
+					"Judge":judge
+					}
 
 		next_page = response.css("td.f a")[2].attrib["href"]
 
