2f0d85f893c07c1cece786320426fd95c506f200
Hinal <hinaljajal@ucla.edu>
2021-12-03 11:00:28 -0800

    updated settings to include pipelines

--- Web Scraping/case_info/case_info/settings.py
+++ Web Scraping/case_info/case_info/settings.py
@@ -29,6 +29,11 @@ ROBOTSTXT_OBEY = True
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 3
 
+ITEM_PIPELINES = {
+    'case_info.pipelines.CasesPipeline': 800,
+    'case_info.pipelines.DatesPipeline': 300,
+}
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
