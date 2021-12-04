c127e00e5017862aa9fba38691a77554d0a012a7
Hinal <hinaljajal@ucla.edu>
2021-12-03 18:59:42 -0800

    trying different data item

--- Web Scraping/case_info/case_info/pipelines.py
+++ Web Scraping/case_info/case_info/pipelines.py
@@ -1,49 +1,52 @@
 from itemadapter import ItemAdapter
 from scrapy.exporters import CsvItemExporter
-from items import CaseItem
+# from items import CaseItem
 from items import DatesItem
+from items import DaItem
 
-class CasesPipeline:
 
-    # def open_spider(self, spider):
-    #     self.file = open('courts.csv', 'wb')
-    #     self.exporter = CsvItemExporter(self.file)
-    #     self.exporter.start_exporting()
+# class CasesPipeline:
 
-    # def close_spider(self, spider):
-    #     self.exporter.finish_exporting()
-    #     self.file.close()
+#     # def open_spider(self, spider):
+#     #     self.file = open('courts.csv', 'wb')
+#     #     self.exporter = CsvItemExporter(self.file)
+#     #     self.exporter.start_exporting()
 
-    # def process_item(self, item, spider):
+#     # def close_spider(self, spider):
+#     #     self.exporter.finish_exporting()
+#     #     self.file.close()
+
+#     # def process_item(self, item, spider):
         
-    #     # if isinstance(item, CaseItem):
-    #     self.exporter.export_item(item)
-    #     return item
+#     #     # if isinstance(item, CaseItem):
+#     #     self.exporter.export_item(item)
+#     #     return item
 
-    def open_spider(self, spider):
-        self.court_to_exporter = {}
+#     def open_spider(self, spider):
+#         self.court_to_exporter = {}
 
-    def close_spider(self, spider):
-        for exporter, csv_file in self.court_to_exporter.values():
-            exporter.finish_exporting()
-            csv_file.close()
+#     def close_spider(self, spider):
+#         for exporter, csv_file in self.court_to_exporter.values():
+#             exporter.finish_exporting()
+#             csv_file.close()
 
-    def _exporter_for_item(self, item):
-        if isinstance(item, CaseItem):
-            adapter = ItemAdapter(item)
-            court = adapter['court']
-            if court not in self.court_to_exporter:
-                csv_file = open(f'{court}.csv', 'wb')
-                exporter = CsvItemExporter(csv_file)
-                exporter.start_exporting()
-                self.court_to_exporter[court] = (exporter, csv_file)
-            return self.court_to_exporter[court][0]
+#     def _exporter_for_item(self, item):
+#         if isinstance(item, CaseItem):
+#             adapter = ItemAdapter(item)
+#             court = adapter['court']
+#             if court not in self.court_to_exporter:
+#                 csv_file = open(f'{court}.csv', 'wb')
+#                 exporter = CsvItemExporter(csv_file)
+#                 exporter.start_exporting()
+#                 self.court_to_exporter[court] = (exporter, csv_file)
+#             return self.court_to_exporter[court][0]
 
-    def process_item(self, item, spider):
-        if isinstance(item, CaseItem):
-            exporter = self._exporter_for_item(item)
-            exporter.export_item(item)
-            return item
+#     def process_item(self, item, spider):
+#         if isinstance(item, CaseItem):
+#             exporter = self._exporter_for_item(item)
+#             exporter.export_item(item)
+#             print(item['name'])
+#             return item
 
 class DatesPipeline:
 
@@ -60,6 +63,26 @@ class DatesPipeline:
         
         if isinstance(item, DatesItem):
             self.exporter.export_item(item)
+            print(item['date'])
             return item
+
+class DaPipeline:
+
+    def open_spider(self, spider):
+        self.file = open('dates_finished.csv', 'wb')
+        self.exporter = CsvItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        
+        if isinstance(item, DaItem):
+            self.exporter.export_item(item)
+            print(item['date'])
+            return item
+
 
            
--- Web Scraping/case_info/case_info/settings.py
+++ Web Scraping/case_info/case_info/settings.py
@@ -30,8 +30,10 @@ ROBOTSTXT_OBEY = True
 DOWNLOAD_DELAY = 3
 
 ITEM_PIPELINES = {
-    'case_info.pipelines.CasesPipeline': 800,
+    # 'case_info.pipelines.CasesPipeline': 800,
     'case_info.pipelines.DatesPipeline': 300,
+    'case_info.pipelines.DaPipeline': 200,
+
 }
 
 # The download delay setting will honor only one of:
