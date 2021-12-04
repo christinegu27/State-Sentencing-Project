13eb1fc99940bd9ee52109bf51f5c7c42349269e
Hinal <hinaljajal@ucla.edu>
2021-12-03 18:00:07 -0800

    debugging 2

--- Web Scraping/case_info/case_info/pipelines.py
+++ Web Scraping/case_info/case_info/pipelines.py
@@ -5,45 +5,45 @@ from items import CaseItem
 
 class CasesPipeline:
 
-    def open_spider(self, spider):
-        self.file = open('courts.csv', 'wb')
-        self.exporter = CsvItemExporter(self.file)
-        self.exporter.start_exporting()
-
-    def close_spider(self, spider):
-        self.exporter.finish_exporting()
-        self.file.close()
-
-    def process_item(self, item, spider):
-        
-        # if isinstance(item, CaseItem):
-        self.exporter.export_item(item)
-        return item
-
     # def open_spider(self, spider):
-    #     self.court_to_exporter = {}
+    #     self.file = open('courts.csv', 'wb')
+    #     self.exporter = CsvItemExporter(self.file)
+    #     self.exporter.start_exporting()
 
     # def close_spider(self, spider):
-    #     for exporter, csv_file in self.court_to_exporter.values():
-    #         exporter.finish_exporting()
-    #         csv_file.close()
-
-    # def _exporter_for_item(self, item):
-    #     if isinstance(item, CaseItem):
-    #         adapter = ItemAdapter(item)
-    #         court = adapter['court']
-    #         if court not in self.court_to_exporter:
-    #             csv_file = open(f'{court}.csv', 'wb')
-    #             exporter = CsvItemExporter(csv_file)
-    #             exporter.start_exporting()
-    #             self.court_to_exporter[court] = (exporter, csv_file)
-    #         return self.court_to_exporter[court][0]
+    #     self.exporter.finish_exporting()
+    #     self.file.close()
 
     # def process_item(self, item, spider):
-    #     if isinstance(item, CaseItem):
-    #         exporter = self._exporter_for_item(item)
-    #         exporter.export_item(item)
-    #         return item
+        
+    #     # if isinstance(item, CaseItem):
+    #     self.exporter.export_item(item)
+    #     return item
+
+    def open_spider(self, spider):
+        self.court_to_exporter = {}
+
+    def close_spider(self, spider):
+        for exporter, csv_file in self.court_to_exporter.values():
+            exporter.finish_exporting()
+            csv_file.close()
+
+    def _exporter_for_item(self, item):
+        if isinstance(item, CaseItem):
+            adapter = ItemAdapter(item)
+            court = adapter['court']
+            if court not in self.court_to_exporter:
+                csv_file = open(f'{court}.csv', 'wb')
+                exporter = CsvItemExporter(csv_file)
+                exporter.start_exporting()
+                self.court_to_exporter[court] = (exporter, csv_file)
+            return self.court_to_exporter[court][0]
+
+    def process_item(self, item, spider):
+        if isinstance(item, CaseItem):
+            exporter = self._exporter_for_item(item)
+            exporter.export_item(item)
+            return item
 
 # class DatesPipeline:
 
